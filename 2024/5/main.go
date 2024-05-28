06f4be208d33ae4938f539f061952b11997e587d
wuzizhongqiu <2669560858@qq.com>
2024-05-28 11:00:18 +0800

    【LeetCode】每日一题 2024_5_28 找出峰值（模拟）

--- 2024/5/main.go
+++ 2024/5/main.go
@@ -156,3 +156,13 @@ func missingRolls(rolls []int, mean int, n int) []int {
 	}
 	return ans
 }
+
+func findPeaks(mountain []int) []int {
+	ans := []int{}
+	for i := 1; i < len(mountain)-1; i++ {
+		if mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1] {
+			ans = append(ans, i)
+		}
+	}
+	return ans
+}
